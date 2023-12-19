ce1765b544f3dcb95a012d8f4bb6f239e767ae0e
MelekErisgin34 <147922709+MelekErisgin34@users.noreply.github.com>
2023-12-19 12:53:09 +0300

    Add files via upload

---
+++ RGB formun gengini tb ile degistirme/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace RGB_formun_gengini_tb_ile_degistirme
+{
+    partial class form_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRed = new System.Windows.Forms.TrackBar();
+            this.tbGreen = new System.Windows.Forms.TrackBar();
+            this.tbBlue = new System.Windows.Forms.TrackBar();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbRed
+            // 
+            this.tbRed.Location = new System.Drawing.Point(52, 51);
+            this.tbRed.Maximum = 255;
+            this.tbRed.Name = "tbRed";
+            this.tbRed.Size = new System.Drawing.Size(389, 45);
+            this.tbRed.TabIndex = 0;
+            this.tbRed.Scroll += new System.EventHandler(this.tbRed_Scroll);
+            // 
+            // tbGreen
+            // 
+            this.tbGreen.Location = new System.Drawing.Point(52, 113);
+            this.tbGreen.Maximum = 255;
+            this.tbGreen.Name = "tbGreen";
+            this.tbGreen.Size = new System.Drawing.Size(389, 45);
+            this.tbGreen.TabIndex = 1;
+            this.tbGreen.Scroll += new System.EventHandler(this.tbGreen_Scroll);
+            // 
+            // tbBlue
+            // 
+            this.tbBlue.Location = new System.Drawing.Point(52, 175);
+            this.tbBlue.Maximum = 255;
+            this.tbBlue.Name = "tbBlue";
+            this.tbBlue.Size = new System.Drawing.Size(389, 45);
+            this.tbBlue.TabIndex = 2;
+            this.tbBlue.Scroll += new System.EventHandler(this.tbBlue_Scroll);
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.ForeColor = System.Drawing.Color.Purple;
+            this.lblRed.Location = new System.Drawing.Point(447, 64);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(35, 13);
+            this.lblRed.TabIndex = 3;
+            this.lblRed.Text = "label1";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.ForeColor = System.Drawing.Color.Purple;
+            this.lblGreen.Location = new System.Drawing.Point(447, 113);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(35, 13);
+            this.lblGreen.TabIndex = 4;
+            this.lblGreen.Text = "label2";
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.ForeColor = System.Drawing.Color.Purple;
+            this.lblBlue.Location = new System.Drawing.Point(447, 175);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(35, 13);
+            this.lblBlue.TabIndex = 5;
+            this.lblBlue.Text = "label3";
+            // 
+            // form_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.tbBlue);
+            this.Controls.Add(this.tbGreen);
+            this.Controls.Add(this.tbRed);
+            this.Name = "form_1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.form_1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbRed;
+        private System.Windows.Forms.TrackBar tbGreen;
+        private System.Windows.Forms.TrackBar tbBlue;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblBlue;
+    }
+}
+
---
+++ RGB formun gengini tb ile degistirme/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGB_formun_gengini_tb_ile_degistirme
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new form_1());
+        }
+    }
+}
